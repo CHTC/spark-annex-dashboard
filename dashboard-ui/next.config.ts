e4d818cc197372d9b0402ee851ac11af7020993c
Matthew Westphall <westphall@wisc.edu>
2026-01-14 17:09:16 -0600

    Update dependencies

--- dashboard-ui/next.config.ts
+++ dashboard-ui/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  basePath: "dashboard",
+//  basePath: "/dashboard",
 };
 
 export default nextConfig;
