7fea4953ba34f488a3181d514e59cc021250e3bf
Matthew Westphall <westphall@wisc.edu>
2026-01-15 12:15:31 -0600

    Add middleware for auth

--- dashboard-api/src/app.py
+++ dashboard-api/src/app.py
@@ -1,30 +1,23 @@
 from typing import Union
 
-from fastapi import FastAPI, Request, Depends
-from auth_handler import read_cilogon_headers
+from fastapi import FastAPI, HTTPException, Request, Depends
+from auth_handler import verify_auth_headers
 import jwt
 
-app = FastAPI(
-#    dependencies=[Depends(read_cilogon_headers)]
-)
+app = FastAPI()
 
-JWKS_URL = "https://cilogon.org/oauth2/certs"
-AUDIENCE = "cilogon:/client_id/534864d355885529ea6033daa9bdf0ec"
-
-def verify_auth_headers(request: Request) -> Union[dict, None]:
-    auth_header = request.headers.get("Authorization")
-    if auth_header:
-        token = auth_header.split(" ")[1]
-        jwks_client = jwt.PyJWKClient(JWKS_URL)
-        signing_key = jwks_client.get_signing_key_from_jwt(token)
-        decoded_token = jwt.decode(token, signing_key, audience=AUDIENCE, algorithms=["RS256"])
-        return decoded_token
-    return "No token provided"
-
+@app.middleware("http")
+async def auth_middleware(request: Request, call_next):
+    auth_info = verify_auth_headers(request)
+    request.state.auth_info = auth_info
+    if auth_info is None:
+        return HTTPException(status_code=401, detail="Unauthorized")
+    response = await call_next(request)
+    return response
 
 @app.get("/")
 def read_root(request: Request) -> dict[str, str]:
-    print(verify_auth_headers(request))
+    print(f"Auth info: {request.state.auth_info}")
     return {
         "message": "Hello, world!",
     }
---
+++ dashboard-api/src/auth_handler.py
@@ -0,0 +1,21 @@
+from fastapi import Request
+import jwt
+from typing import Union
+
+JWKS_URL = "https://cilogon.org/oauth2/certs"
+AUDIENCE = "cilogon:/client_id/534864d355885529ea6033daa9bdf0ec"
+
+def verify_auth_headers(request: Request) -> Union[dict, None]:
+    auth_header = request.headers.get("Authorization")
+    if auth_header:
+        try:
+            token = auth_header.split(" ")[1]
+            jwks_client = jwt.PyJWKClient(JWKS_URL)
+            signing_key = jwks_client.get_signing_key_from_jwt(token)
+            decoded_token = jwt.decode(token, signing_key, audience=AUDIENCE, algorithms=["RS256"])
+            print(f"decoded auth token successfully: {decoded_token}")
+            return decoded_token
+        except Exception as e:
+            print(f"Error verifying token: {e}")
+            return None
+    return None
